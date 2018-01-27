d2af422fc7d2b7bf19432e39a74c20e3af9cc582
Robert Stow <robstow94@gmail.com>
2018-01-27 17:14:49 +0000

    Added preemiptible to workers & it'll start up 8 workers.

--- deployment.sh
+++ deployment.sh
@@ -11,19 +11,20 @@ sudo git clone https://github.com/portsoc/clocoss-master-worker
 cd clocoss-master-worker
 sudo npm install
 
+#run srever
+npm run server $key &
+
 #create N client
-for i in `seq 1 3`
+for i in `seq 1 8`
 do
   gcloud compute instances create --machine-type f1-micro --zone europe-west1-c \
   --metadata serverIp="$serverIp",key="$key" --scopes compute-rw\
   --metadata-from-file startup-script=../startup-script.sh \
+  --preemptible \
   robstow-worker-$i
 done
 
-#run srever
-npm run server $key
-
-for i in `seq 1 3`
+for i in `seq 1 8`
 do
 gcloud compute instances delete robstow-worker-$i --zone europe-west1-c
 done
